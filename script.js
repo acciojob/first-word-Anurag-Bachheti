7459a82214e93d396556437b4bfa5a95f17ca6cf
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-03-25 17:22:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstWord(s) {
-  // your code here
+	let newS = s.trim().split(" ");
+	if(newS === ("")){
+		return("")
+	}else{
+	return newS[0]
+	}
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
